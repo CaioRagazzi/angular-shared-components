7bf256c4d40d97de2875ec5610b77bae92ede1de
Caio Ragazzi <ca.ragazzi@gmail.com>
2024-01-28 19:08:58 +0000

    feat: adding router

--- projects/blog-caio/src/app/app-routing.module.ts
+++ projects/blog-caio/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./about/about.module').then((m) => m.AboutModule),
+  },
+  {
+    path: 'contacts',
+    loadChildren: () =>
+      import('./contacts/contacts.module').then((m) => m.ContactsModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/blog-caio/src/app/header/header.module.ts
+++ projects/blog-caio/src/app/header/header.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HeaderComponent } from './header.component';
 import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
 import { SocialIconsComponent } from './social-icons/social-icons.component';
 
 @NgModule({
-  declarations: [HeaderComponent, LogoComponent, SocialIconsComponent],
-  imports: [FontAwesomeModule],
+  declarations: [
+    HeaderComponent,
+    LogoComponent,
+    SocialIconsComponent,
+    NavComponent,
+  ],
+  imports: [FontAwesomeModule, RouterModule],
   providers: [],
   exports: [HeaderComponent],
 })
