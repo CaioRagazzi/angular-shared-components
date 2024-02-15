0f5e8cf076205182aded36d60a7717eb43d97f67
CaioRagazzi <ca.ragazzi@gmail.com>
2024-02-15 20:12:19 +0000

    adjust card component

--- projects/ngx-shared-components/src/lib/card/ca-card/ca-card.component.ts
+++ projects/ngx-shared-components/src/lib/card/ca-card/ca-card.component.ts
@@ -1,36 +1,14 @@
 import {
-  AfterContentInit,
   Component,
-  ContentChildren,
   Input,
-  QueryList,
-  TemplateRef,
 } from '@angular/core';
-import { CaTemplate } from '../../shared/directives/caTemplate';
 
 @Component({
   selector: 'lib-ca-card',
   templateUrl: './ca-card.component.html',
   styleUrl: './ca-card.component.scss',
 })
-export class CaCardComponent implements AfterContentInit {
+export class CaCardComponent {
   @Input() title = '';
   @Input() previewText = '';
-
-  imageTemplate: TemplateRef<any> | undefined;
-
-  @ContentChildren(CaTemplate) templates: QueryList<CaTemplate> | undefined;
-
-  ngAfterContentInit() {
-    (this.templates as QueryList<CaTemplate>).forEach((item) => {
-      switch (item.getType()) {
-        case 'image':
-          this.imageTemplate = item.template;
-          break;
-
-        default:
-          break;
-      }
-    });
-  }
 }
